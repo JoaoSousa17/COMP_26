fb35fef8dc62856d7e321bdd0be61c58c910cdd6
JoaoSousa17 <joaopedro.2004@hotmail.com>
2026-05-07 03:29:23 +0100

    .

--- test-public/pt/up/fe/comp/cp2/core/ollir/entitiesandops/AssignmentsOllirTest.java
+++ test-public/pt/up/fe/comp/cp2/core/ollir/entitiesandops/AssignmentsOllirTest.java
@@ -1,14 +1,15 @@
 package pt.up.fe.comp.cp2.core.ollir.entitiesandops;
 
 import org.junit.Test;
-import org.specs.comp.ollir.*;
-import org.specs.comp.ollir.inst.*;
+import org.specs.comp.ollir.ClassUnit;
+import org.specs.comp.ollir.Method;
+import org.specs.comp.ollir.Operand;
+import org.specs.comp.ollir.inst.AssignInstruction;
 import org.specs.comp.ollir.inst.utils.InstTypeUtils;
 import org.specs.comp.ollir.type.BuiltinKind;
 import org.specs.comp.ollir.type.BuiltinType;
 import org.specs.comp.ollir.type.ClassType;
 import pt.up.fe.comp.test.env.OllirTestEnv;
-import pt.up.fe.comp2026.optimization.OptUtils;
 
 public class AssignmentsOllirTest extends OllirTestEnv {
     private static final String BASE_PATH = "pt/up/fe/comp/cp2/core/ollir/entitiesandops/jmm/";
@@ -59,37 +60,6 @@ public class AssignmentsOllirTest extends OllirTestEnv {
 
         assertTrue("a must be i32", assignToA.getDest() instanceof Operand op && BuiltinType.is(op.getType(), BuiltinKind.INT32));
         var rhsTypeOpt = InstTypeUtils.getType(assignToA);
-        assertTrue("Should be able to determine type of rhs assignment to 'a'", rhsTypeOpt.isPresent());
-        assertTrue("RHS of assignment to 'a' must be a i32", BuiltinType.is(rhsTypeOpt.orElseThrow(), BuiltinKind.INT32));
-    }
-
-    @Test
-    public void testAssignField() {
-        var method = toOllir("AssignField.jmm", "method");
-
-        var putField = assertInstExists(PutFieldInstruction.class, method);
-        var assignToFieldOpt = putField.stream().filter(pf -> pf.getField().getName().equals("a")).findFirst();
-        assertTrue("There should be an assignment to field 'a'", assignToFieldOpt.isPresent());
-        var assignToField = assignToFieldOpt.orElseThrow();
-
-        assertTrue("Field a must be i32", BuiltinType.is(assignToField.getField().getType(), BuiltinKind.INT32));
-        assertTrue("RHS of assignment to 'field' must be a i32", BuiltinType.is(assignToField.getValue().getType(), BuiltinKind.INT32));
-    }
-
-    @Test
-    public void testAssignFromField() {
-        var method = toOllir("AssignFromField.jmm", "method");
-
-
-        var getfields = assertInstExists(GetFieldInstruction.class, method);
-        var getFieldOpt = getfields.stream().filter(a -> a.getField().getName().equals("b")).findFirst();
-        assertTrue("There should be a getfield to field 'b'", getFieldOpt.isPresent());
-        assertTrue("Accessed field b must be i32", getFieldOpt.orElseThrow().getField().getType() instanceof BuiltinType bt && BuiltinType.is(bt, BuiltinKind.INT32));
-
-        var assign = assertInstExists(AssignInstruction.class, method);
-        var assignFromFieldOpt = assign.stream().filter(a -> a.getDest() instanceof Operand op && op.getName().equals("a")).findFirst();
-        assertTrue("There should be an assignment to variable 'a'", assignFromFieldOpt.isPresent());
-        var rhsTypeOpt = InstTypeUtils.getType(assignFromFieldOpt.orElseThrow());
         assertTrue("Should be able to determine type of rhs assignment to 'a'", rhsTypeOpt.isPresent());
         assertTrue("RHS of assignment to 'a' must be a i32", BuiltinType.is(rhsTypeOpt.orElseThrow(), BuiltinKind.INT32));
     }
